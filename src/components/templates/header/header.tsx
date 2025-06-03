a618d59f3ce7dc3d05299f6e33c6adf688a3c927
Daniel Osterbro <daniel.oesterbro@gmail.com>
2025-06-03 21:30:43 +0200

    Update header.tsx

--- src/components/templates/header/header.tsx
+++ src/components/templates/header/header.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from 'next-i18next';
 import Link from 'next/link';
 
 import MobileLogo from '@icons/bonelli-mobile.svg';
-import DesktopLogo from '@icons/bonelli.svg';
+import DesktopLogo from '@icons/solar_boxlogo_blue_rgb.png';
 import { LanguageSelector } from '@src/components/features/language-selector';
 
 export const HEADER_HEIGHT = 60;
